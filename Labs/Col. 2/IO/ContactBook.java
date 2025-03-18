f4f1f2ebd2d0341db70a5e696d3c52308caafb9c
Irfan Islam <patweryif@gmail.com>
2025-03-18 11:15:50 +0300

    Update and rename Task2.java to ContactBook.java

--- Labs/Col. 2/IO/ContactBook.java
+++ Labs/Col. 2/IO/ContactBook.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.Scanner;
-public class Task2 {
+public class ConactBook {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String name = "", address;
